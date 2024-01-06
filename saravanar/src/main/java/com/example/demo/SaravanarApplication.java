5dcc6b066b45b2fb5f01d7370df0d06f8a0cf401
sanje <sanje@sanjeev-PC>
2024-01-06 19:41:08 +0530

    initial commit

---
+++ saravanar/src/main/java/com/example/demo/SaravanarApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaravanarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaravanarApplication.class, args);
+		jchsduh
+	}
+
+}
